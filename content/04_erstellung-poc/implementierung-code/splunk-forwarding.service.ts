b1d44fa3817d8bd7ab28e1a8d71e2fb56e612b57
JapuDCret <JapuDCret@users.noreply.github.com>
2021-03-06 21:27:29 +0100

    [ELAB-436] [ELAB-437] [ELAB-362] final work^1

--- content/04_erstellung-poc/implementierung-code/splunk-forwarding.service.ts
+++ content/04_erstellung-poc/implementierung-code/splunk-forwarding.service.ts
@@ -1,11 +1,7 @@
 export class SplunkForwardingService {
-  // ...
-  
-  private batchQueue: SplunkEntry[];
+  private batchQueue: SplunkEntry[] = [];
   
   constructor(private http: HttpClient) {
-    // ...
-	
     // Prüfe alle 5s ob Daten zum Weiterleiten existieren
     interval(5000)
       .subscribe(() => {
@@ -15,16 +11,24 @@ export class SplunkForwardingService {
 
         this.sendBatch(batch)
           .pipe(
-            // 5 Versuche mit einer Wartezeit von jew. 15s
+            // 5 Versuche mit einer Wartezeit von jeweils 15s
             retryWhen(errors => errors.pipe(delay(15000), take(5)))
           )
           .subscribe();
       });
   }
 
+  public forwardEvents(entries: SplunkEntry[]): void {
+    for(const entry of entries) {
+      // Füge Kontextinformationen hinzu
+      entry.event.path = window.location.href;
+      entry.event.shoppingCartId = window.customer.shoppingCartId;
+
+      this.batchQueue.push(entry);
+    }
+  }
+
   private sendBatch(batch: SplunkEntry[]): Observable<void> {
-    // ...
-    
     return this.http.post<void>(this.logBatchServiceUrl, batch);
   }
 }
